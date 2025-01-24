74fa2afa110260d0b358a2eaa2040482e9a58cb5
jhonatann-per <jhonatanpereira152@gmail.com>
2025-01-23 22:52:07 -0300

    Criação do campo de tarefas e sua estilização

--- src/components/Cadastro/Cadastro.jsx
+++ src/components/Cadastro/Cadastro.jsx
@@ -2,10 +2,28 @@ import styles from './Cadastro.module.css'
 
 export const Cadastro = () =>{
     return (
-        <footer className={styles.footer}>
+        <div className={styles.cadastro}>
             <form>
-                <textarea name="cadastreTask" placeholder='Escreva sua Tarefa'/>
+                <textarea name="cadastreTask" placeholder='Adicione uma nova tarefa'/>
+                <button type="submit">Criar</button>
             </form>
-        </footer>
+
+            <div className={styles.tarefas}>
+                <div className={styles.status}>
+                    <div><span className={styles.span1}>Tarefas criadas </span></div>
+                    <div><span className={styles.span2}>Concluidas </span></div>
+                </div>
+            </div>
+            <footer className={styles.footer}>
+                    <div className={styles.task}>
+                        <input type="checkbox"/>
+                        
+                        <span>Integer urna interdum massa libero 
+                        auctor neque turpis turpis semper. 
+                        Duis vel sed fames integer.</span>
+                    </div>
+            </footer>
+            
+        </div>
     )
 }
